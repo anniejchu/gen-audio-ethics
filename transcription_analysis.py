1f0c9cdc973331b2300fc0da4d527972f525b8e9
William Agnew <wagnew3@cs.washington.edu>
2024-05-18 14:29:38 -0400

    io optimization

--- transcription_analysis.py
+++ transcription_analysis.py
@@ -158,7 +158,7 @@ class Transcription_Analyzer:
             output = io.BytesIO()
             StreamWriter = codecs.getwriter('utf-8')  # here you pass the encoding
             wrapper_file = StreamWriter(output)
-            with redirect_stdout(output), YoutubeDL(ydl_opts) as ydl:
+            with redirect_stdout(wrapper_file), YoutubeDL(ydl_opts) as ydl:
                 error_code=ydl.download([f'https://www.youtube.com/watch?v={youtube_id}'])
                 # try:
                 #     error_code=ydl.download([f'https://www.youtube.com/watch?v={youtube_id}'])

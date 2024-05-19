3d605b93db8dbcb27e4255b68793dbdb1ed117f0
William Agnew <wagnew3@cs.washington.edu>
2024-05-19 01:20:39 -0400

    io optimization

--- transcription_analysis.py
+++ transcription_analysis.py
@@ -372,7 +372,7 @@ if __name__ == '__main__':
             pass
 
     #analyzer.get_audio_info_unk("/Users/williamagnew/eclipse-workspace/gen-audio-ethics/Flume - Never Be Like You feat. Kai [Ly7uj0JwgKg].m4a", "Oh, can't you see I made, I made a mistake Please just look me in my face Tell me everything's okay Cause I got this")
-    # analyzer.get_audio_info_youtube("Ly7uj0JwgKg", options.save_loc, 120, 130, [], None)
+    analyzer.get_audio_info_youtube("Ly7uj0JwgKg", options.save_loc, 120, 130, [], None)
     # exit()
     
     with open(options.youtube_ids, newline='') as f:

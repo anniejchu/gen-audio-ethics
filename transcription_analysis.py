6bbd72cbaf9bde79b4b41b4e5898cfc3857ac632
William Agnew <wagnew3@cs.washington.edu>
2024-05-21 18:47:48 -0400

    use downlaoded yt audio

--- transcription_analysis.py
+++ transcription_analysis.py
@@ -213,6 +213,8 @@ class Transcription_Analyzer:
                 # If this fails use whisper
                 
                 s_time=time.time()
+                transcript="sgf seefgh"
+                langauge="en"
                 if transcript is None:
                     result=self.whisper_transcribe(whisper_pipe, os.path.join(save_dir, file_name))#whisper_transcriber.transcribe(os.path.join(save_dir, file_name[:-3]+'wav'))#
                     transcript=result['text']
@@ -370,8 +372,7 @@ if __name__ == '__main__':
             pass
 
     #analyzer.get_audio_info_unk("/Users/williamagnew/eclipse-workspace/gen-audio-ethics/Flume - Never Be Like You feat. Kai [Ly7uj0JwgKg].m4a", "Oh, can't you see I made, I made a mistake Please just look me in my face Tell me everything's okay Cause I got this")
-    if options.info_transcribe:
-        analyzer.get_audio_info_youtube("ZZzwHGqVw0c", options.save_loc, 120, 130, [], None, options.info_transcribe)
+    analyzer.get_audio_info_youtube("ZZzwHGqVw0c", options.save_loc, 120, 130, [], None, options.info_transcribe)
     
     with open(options.youtube_ids, newline='') as f:
         reader = csv.reader(f)

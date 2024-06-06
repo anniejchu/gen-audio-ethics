2c143024df4b1361c7c868e53be93c5acf56be90
William Agnew <wagnew3@cs.washington.edu>
2024-06-06 01:10:53 -0400

    copyirght plotting

--- copyright_holders_plot.py
+++ copyright_holders_plot.py
@@ -4,18 +4,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import compress_pickle as pickle
+from datetime import datetime
 
+def parse_credits(credits_list):
+    names=[]
+    for credits_dict in credits_list:
+        for artist in credits_dict['artists']:
+            names.append(artist['name'])
+    return names
+        
 audioset_meta_location="/gscratch/scrubbed/wagnew2/audioset_info_collated.lz4"
+audioset_song_infos=pickle.load(audioset_meta_location)
+
+audioset_meta_location="/gscratch/scrubbed/wagnew2/audioset_metadata_collated.lz4"
 audioset_metas=pickle.load(audioset_meta_location)
+
 num_print=5
 printed=0
-for fid in audioset_metas:
-    for k in audioset_meta[fid]:
-        print(k, audioset_meta[fid][k])
-        print("---------------------------------")
-    printed+=1
-    if printed>num_print:
-        exit()
+all_names=[]
+file_ids=list(set(list(audioset_metas.keys())+list(audioset_song_infos.keys())))
+for fid in tqdm(file_ids):
+    names=[]
+    
+    if fid in audioset_song_infos:
+        release_date=datetime.strptime(audioset_song_infos[fid]['music_info']['top_hit_full_info']['song']['release_date'], '%Y-%m-%d').date()
+        if release_date.year<=2016:
+            
+            
+            names.append(audioset_song_infos[fid]['music_info']['top_hit_full_info']['song']['artist_names'])
+            for k in audioset_song_infos[fid]['music_info']['top_hit_full_info']['song']:
+                info_list=audioset_song_infos[fid]['music_info']['top_hit_full_info']['song'][k]
+                if isinstance(info_list, list):
+                    for info_dict in info_list:
+                        if isinstance(info_dict, dict) and 'relationship_type' in info_dict and 'name' in info_dict:
+                            names.append(info_dict['name'])
+            
+            names+=parse_credits(audioset_song_infos[fid]['music_info']['song_credits'])
+            names+=parse_credits(audioset_song_infos[fid]['music_info']['xtra_credits'])
+        else:
+            continue
+    if fid in audioset_metas:
+        names.append(audioset_metas[fid]['yt_info']['uploader'])
+        
+    names=[name.lower() for name in names]
+    names=list(set(names))
+    all_names+=names
+    
+pickle.dump(all_names, open("/gscratch/scrubbed/wagnew2/all_artist_names_audioset.lz4", 'wb'))
+    
+        # for k in audioset_song_infos[fid]['music_info']:
+        #     print(k, audioset_song_infos[fid][k])
+        #     print("---------------------------------")
+        # printed+=1
+        # if printed>num_print:
+        #     exit()
 
 # # load lakh
 # lakh_copyholders="/Users/williamagnew/eclipse-workspace/gen-audio-ethics/lakh_copyright.txt"

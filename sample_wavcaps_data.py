b0e6254c1b02f2d8372e93f461ea218318095a11
William Agnew <wagnew@andrew.cmu.edu>
2025-05-21 18:58:17 -0400

    sample random wavcaps

--- sample_wavcaps_data.py
+++ sample_wavcaps_data.py
@@ -2,9 +2,9 @@ import os
 import random
 import shutil
 
-data_paths=["/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/SoundBible/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/AudioSet_SL_flac",
-            "/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/SoundBible/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/BBC_Sound_Effects_flac",
-            "/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/SoundBible/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/FreeSound_flac",
+data_paths=["/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/AudioSet_SL/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/AudioSet_SL_flac",
+            "/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/BBC_Sound_Effects/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/BBC_Sound_Effects_flac",
+            "/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/FreeSound/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/FreeSound_flac",
             "/data/user_data/wagnew/wav-caps/WavCaps/Zip_files/SoundBible/mnt/fast/nobackup/scratch4weeks/xm00178/WavCaps/data/waveforms/SoundBible_flac"]
 save_folder="/data/user_data/wagnew/wav-caps/WavCaps/sample"
 
